45445414b0440fca5f6f09c9ba1dc07888be65f2
chris-jburke <cjburke04@gmail.com>
2020-01-12 19:05:52 -0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,19 @@
 //main.js
 console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+const cards = ["queen", "queen", "king", "king"];
+let cardOne = cards[0];
+const cardsInPlay =[];
+cardsInPlay.push(cardOne);
+console.log("User flipped a " + cardOne);
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped a " + cardTwo)
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
